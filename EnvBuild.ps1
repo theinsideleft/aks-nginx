a8cd52d60554857e1707b2e84500859610299ef8
Darren Taylor <darren@darrentaylor.net>
2023-09-11 11:55:56 +0100

    initial commit

--- EnvBuild.ps1
+++ EnvBuild.ps1
@@ -1,65 +1,44 @@
-## Required Software
+﻿##
+#Need to have Az CLI, Helm and Kubectl installed
+#Need to have the 
+##
 
-Ensure you have an Azure subscription you can use. If you have an MSDN subscription don't forget to activate your monthly Azure Subscription you get with that MSDN.
-
-You will also need the following software installed
-
-1. [Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli)
-2. [Helm](https://helm.sh/docs/intro/install/)
-3. [Kubectl](https://kubernetes.io/docs/tasks/tools/)
-4. Valid TLS/SSL Certificate. One can be obtained from [LetsEncrypt](https://letsencrypt.org/) - see my previous post [here](https://darrentaylor.net/post/static-website-azure-blob-storage/)
-
-I develop on a windows machine and use the Windows PowerShell ISE for development. I am sure there are more funkier IDEs, but this does the job for me! For ease of simplicity, I will create one large PowerShell script that can be executed line by line so you can see what is going on.
-
-## Variables
-
-Let’s start with declaring the variables we will use and that can be customised based on your needs.
-
-```powershell
-## Set Variables
-
-$AZ_RESOURCE_GROUP="demo-nginx" #Your resource group
-$AZ_LOCATION="northeurope" #What location you want your resources created in
-$AZ_CLUSTER_NAME="demo-nginx-aks" #Name of your AKS Cluster
-$AZ_VNET_NAME="demo-nginx-vnet" #Name of your VNET
-$AZ_VNET_CIDR="10.222.0.0/16" #CIDR of your VNET
-$AZ_AKS_CIDR="10.222.0.0/22" #CIDR of your AKS Cluster Nodes
-$AZ_AKS_NAME="subnet-aks" 
-$AZ_SVC_LB_CIDR="10.222.4.0/28" #CIDR of your Load Balancer
+############
+# Set Variables
+##############
+$AZ_RESOURCE_GROUP="demo-nginx"
+$AZ_LOCATION="northeurope"
+$AZ_CLUSTER_NAME="demo-nginx-aks"
+$AZ_VNET_NAME="demo-nginx-vnet"
+$AZ_VNET_CIDR="10.222.0.0/16"
+$AZ_AKS_CIDR="10.222.0.0/22"
+$AZ_AKS_NAME="subnet-aks"
+$AZ_SVC_LB_CIDR="10.222.4.0/28"
 $AZ_SVC_LB_NAME="subnet-lb"
-$AZ_AKS_Service_CIDR="10.223.0.0/16" #CIDR of the AKS Services
-$AZ_AKS_DNS_CIDR="10.223.0.10" #IP address of the internal AKS DNS Server
-$AZ_USER_ASSIGNED_IDENTITY_NAME="demo-nginx-managed-id" #Managed Identity name. Another Managed identity gets created in node pool resource group when creating the AKS cluster
-$AZ_SERVICE_ACCOUNT_NAME="workload-identity-sa" # Workflow Identity service account that is created in the AKS Cluster
-$AZ_SERVICE_ACCOUNT_NAMESPACE="default" #Namespace to use for your applications and service account
-$AZ_FEDERATED_IDENTITY_CREDENTIAL_NAME="demo-nginx-federated-credential" #Federated identity credential name that will be linked with the workflow service account
-$AZ_KeyVault_Name="demo-nginx-keyvault" #Keyvault name
-$AZ_PublicIP_Name="demo-nginx-ingress-pip" #Public IP address name
-$AZ_DNS_Label="demo-nginx" #DNS Label to attach to the public IP address
-$AZ_IP_Address_Allow="" #Your IP address to whitelist in the network settings
+$AZ_AKS_Service_CIDR="10.223.0.0/16"
+$AZ_AKS_DNS_CIDR="10.223.0.10"
+$AZ_PublicIP_Name="demo-nginx-ingress-pip"
+$AZ_DNS_Label="demo-nginx"
+
+$AZ_USER_ASSIGNED_IDENTITY_NAME="demo-nginx-managed-id" #just used for accesing azure resources such as appconfig - so maybe rename. Another managed identity gets created in node pool resource group
+
+$AZ_SUBSCRIPTION=""
+$AZ_SERVICE_ACCOUNT_NAME="workload-identity-sa"
+$AZ_SERVICE_ACCOUNT_NAMESPACE="default"
+$AZ_FEDERATED_IDENTITY_CREDENTIAL_NAME="demo-nginx-federated-credential"
+
+#Keyvault where TLS cert is stored
+$AZ_KeyVault_Name="demo-nginx-kv"
+$AZ_KeyVault_Admin="" #Your azure account so you can add the certificate to the key vault
+$AZ_IP_Address_Allow=""#Your IP address
 $AZ_CertificateName="" #Your Certificate name for TLS termination
-$AZ_CertificatePath="" #Path to your TLS certificate. The cert needs to be in pfx format.
-$AZ_KeyVault_Certificates_Officer="" #Your azure account so you can add the certificate to the key vault
-```
+$AZ_CertificatePath="" #Path to your TLS certficate. The cert needs to be in pfx format.
 
-## Account and Subscription
+#Login to Azure - for the subscription you are using make sure you have owner rights
+#az login
+#az account set -s $AZ_SUBSCRIPTION
 
-Open up your IDE/Terminal of choice and login into Azure.
-
-```powershell
-az login
-az account show
-```
-
-Then set your default account to the subscription you want to use. Make sure you have the correct permission on the subscription to create resources.
-
-```powershell
-az account set -s {Subscription Id}
-```
-
-## Create Resources
-
-```powershell
+#Create Resource Group
 az group create --resource-group $AZ_RESOURCE_GROUP --location $AZ_LOCATION
 
 # Create Vnet
@@ -74,7 +53,9 @@ az network vnet subnet create --resource-group $AZ_RESOURCE_GROUP --vnet-name $A
 #Add the service endpoints needed for all services to the AKS Subnet
 az network vnet subnet update -g $AZ_RESOURCE_GROUP -n $AZ_AKS_NAME --vnet-name $AZ_VNET_NAME --service-endpoints Microsoft.KeyVault
 
-#Create Keyvault for storing your TLS Certificate
+#Purge keyvault if created before
+#az keyvault purge --name $AZ_KeyVault_Name --no-wait
+
 az keyvault create -l $AZ_LOCATION -g $AZ_RESOURCE_GROUP -n $AZ_KeyVault_Name --enable-rbac-authorization --sku Standard --default-action Deny
 
 #Get the scope to the Key Vault.
@@ -92,21 +73,18 @@ az keyvault network-rule add -n $AZ_KeyVault_Name -g $AZ_RESOURCE_GROUP --ip-add
 #Grant your account access to key vault to import certificates
 az role assignment create --assignee $AZ_KeyVault_Admin --role "Key Vault Administrator" --scope $AZ_KeyVault_Scope
 
-```
+#Import your TLS Cert to your keyvault
+az keyvault certificate import --vault-name $AZ_KeyVault_Name -n $AZ_CertificateName -f $AZ_CertificatePath
 
-You should now have your keyvault created and limited access to the VNET and your IP address. You can in the Azure portal.
 
-Next steps are to create the AKS cluster with the azure-keyvault-secrets-provider enabled. This will allow pods on your cluster access your key vault secrets.
-More information can be found [here](https://learn.microsoft.com/en-us/azure/aks/csi-secrets-store-driver)
-
-```powershell
 #Create Public IP
 az network public-ip create -g $AZ_RESOURCE_GROUP -n $AZ_PublicIP_Name --allocation-method Static --sku Standard --dns-name $AZ_DNS_Label
 
-#Get that Public IP to use later in the Ingress setup
+#Get that Public IP to use in AKS Cluster
 $AZ_LB_IP=$(az network public-ip show -g $AZ_RESOURCE_GROUP -n $AZ_PublicIP_Name -o tsv --query ipAddress)
 
-#Create Azure Kubernetes Service cluster. Note the VM size and also the azure-keyvault-secrets-provider addon for accessing the TLS certificate
+
+#Create Azure Kubernetes Service cluster.
 az aks create --resource-group $AZ_RESOURCE_GROUP --name $AZ_CLUSTER_NAME --generate-ssh-keys --vm-set-type VirtualMachineScaleSets `
   --node-vm-size "Standard_B2s" `
   --load-balancer-sku standard `
@@ -119,22 +97,15 @@ az aks create --resource-group $AZ_RESOURCE_GROUP --name $AZ_CLUSTER_NAME --gene
   --node-count 3 `
   --zones 1 `
   --service-cidr $AZ_AKS_Service_CIDR `
-  --dns-service-ip $AZ_AKS_DNS_CIDR `
+  --dns-service-ip $AZ_AKS_DNS_CIDR
 
-```
 
-To learn more about the Azure Keyvault Secrets Provider see [here](https://learn.microsoft.com/en-us/azure/aks/csi-secrets-store-driver)
-
-Once this command completes you should have a running AKS cluster with managed identity and the azure-keyvault-secrets-provider enabled.
-
-We will now create a managed identity - see [here](https://learn.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/) for more information on managed identities.
-
-```powershell
 #Create the Managed Identity
 az identity create --name $AZ_USER_ASSIGNED_IDENTITY_NAME --resource-group $AZ_RESOURCE_GROUP --location $AZ_LOCATION --subscription $AZ_SUBSCRIPTION
 
 #Get the Client ID of the managed Identity
 $AZ_USER_ASSIGNED_CLIENT_ID=$(az identity show -n $AZ_USER_ASSIGNED_IDENTITY_NAME -g $AZ_RESOURCE_GROUP --query "clientId" -otsv)
+
 
 #Get the AKS OIDC_Issuer URl
 $AZ_AKS_OIDC_ISSUER=$(az aks show -n $AZ_CLUSTER_NAME -g $AZ_RESOURCE_GROUP --query oidcIssuerProfile.issuerUrl -o tsv)
@@ -147,36 +118,27 @@ $AZ_KeyVault_Scope=$(az keyvault show --resource-group $AZ_RESOURCE_GROUP --name
 
 az role assignment create --role "Key Vault Administrator" --assignee $AZ_USER_ASSIGNED_CLIENT_ID --scope $AZ_KeyVault_Scope 
 
-#Need to give the managed identity the delegated permissions to the resource group the public IP address resides in
+#Need to give the managed idenity the delegated permissions to the resource group the public IP address resides in
 #Give the managed id reader role to the resource group
 $AZ_RESOURCE_GROUP_SCOPE=$(az group show -n $AZ_RESOURCE_GROUP --query id -o tsv)
 $AZ_Client_Identity=$(az aks show -n $AZ_CLUSTER_NAME -g $AZ_RESOURCE_GROUP --query identity.principalId -o tsv)
 
 az role assignment create --assignee $AZ_Client_Identity --role "Network Contributor" --scope $AZ_RESOURCE_GROUP_SCOPE
 
-```
-
-We will now get the AKS Cluster credentials and add it to our local kubeconfig
-
-```powershell
 
 #Set the AKS Cluster
 az aks get-credentials -n $AZ_CLUSTER_NAME -g $AZ_RESOURCE_GROUP
 kubectl config use-context $AZ_CLUSTER_NAME
 
-#Create ingress namespace - can be whatever you want to call it
+#Create ingress namespace
 kubectl create namespace ingress-basic
 
+    
 #Get the Azure Tenant ID for your subscription
 $AZ_AZURE_TENANT_ID=$(az account show -s $AZ_SUBSCRIPTION --query tenantId -otsv)
 
-```
 
-Now we are helming so install Helm and add it to your Path environment variables. When installed and ready to use continue with the next step
-
-```powershell
-
-#Install the mutating webhook for the Azure Workload Identity
+#Install the mutating webhook for the Workload Identity
 helm repo add azure-workload-identity https://azure.github.io/azure-workload-identity/charts
 helm repo update
 helm install workload-identity-webhook azure-workload-identity/workload-identity-webhook `
@@ -184,7 +146,7 @@ helm install workload-identity-webhook azure-workload-identity/workload-identity
    --create-namespace `
    --set azureTenantID="$AZ_AZURE_TENANT_ID"
 
-#Create the workload identity service account in AKS
+#Create the workload identity service acccount in AKS
 $ServiceAccountString = @"
 apiVersion: v1
 kind: ServiceAccount
@@ -197,12 +159,8 @@ metadata:
 
 $ServiceAccountString | kubectl apply -f -
 
-```
-
-Now get the Client ID of the Azure Keyvault secret provider managed id. I tried using the one I created previously but that did not work. The gremlins in Azure!
-And give that client ID access to the keyvault - you can reduce this permission level
-
-```powershell
+#Check that the service account was created
+kubectl get serviceAccounts
 
 #Get the Client ID of the azurekyvaultsecrtepprovider managed id
 $AZ_AKYSP_ClientId= $(az aks show -n $AZ_CLUSTER_NAME -g $AZ_RESOURCE_GROUP --query addonProfiles.azureKeyvaultSecretsProvider.identity.clientId --output tsv)
@@ -210,13 +168,8 @@ $AZ_AKYSP_ClientId= $(az aks show -n $AZ_CLUSTER_NAME -g $AZ_RESOURCE_GROUP --qu
 #Give that client ID the rights to Keyvault
 az role assignment create --role "Key Vault Administrator" --assignee $AZ_AKYSP_ClientId --scope $AZ_KeyVault_Scope 
 
-```
 
-We are now going to create the Secret Provider Class. See [here](https://learn.microsoft.com/en-us/azure/aks/csi-secrets-store-driver)
-
-```powershell
-
-#Create the SecretProviderClass using Client ID to access your key vault
+#Create the SecretProviderClass using workload identity to access your key vault
 
 $SecretProviderClass = @"
 apiVersion: secrets-store.csi.x-k8s.io/v1
@@ -249,13 +202,8 @@ spec:
     tenantId: "${AZ_AZURE_TENANT_ID}"
 "@
 
+
 $SecretProviderClass | kubectl apply -f -
-
-```
-
-Now installing the Nginx Controller
-
-```powershell
 
 #Need to have Helm installed to install the nginx ingress
 # Add the ingress-nginx repository
@@ -293,20 +241,12 @@ helm install ingress-nginx ingress-nginx/ingress-nginx `
     --set controller.extraVolumeMounts[0].mountPath="/mnt/secrets-store" `
     --set controller.extraVolumeMounts[0].readOnly="true"
 
-```
+    #That is your AKS cluster setup so not we move onto applications
 
-That is your AKS cluster setup so now we move onto applications to test the cluster access via HTTPS
-
-If you have cloned this repo, you will have an Apps directory containing 2 K8S yaml files.
-Update the aks-hello-world-ingress.yaml to use the correct hostname.
-
-Make sure you are in the root directory where you cloned the repo and run the following commands
-
-```powershell
+#Review the yaml files into create a hello world test application
+#Change into the root directory of the script
 
 kubectl apply -f .\Apps\aks-hello-world-deployment.yaml
 kubectl apply -f .\Apps\aks-hello-world-ingress.yaml
 
-```
 
-Once those 2 commands have completed you should be able to browse to your https://yourdomain/hello-world-one and view the site over HTTPS.
